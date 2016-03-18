de27d8bab9d9ad6c4cf4808fc3c5995697d99636
Kwonsoo <kwonsoo.chae@gmail.com>
2016-03-18 20:46:57 +0900

    more reduced

---
+++ negative/reducer1/reduced/grep-2.5.1.c/12.c.reduced.c
@@ -0,0 +1,7 @@
+int xmalloc_p, prepend_default_options_tmp___1;
+prepend_default_options_tmp___2prepend_default_options() {
+  prepend_default_options_tmp___1 = prepend_args();
+  xmalloc_p = malloc(prepend_default_options_tmp___1);
+  int ex = xmalloc_p;
+  airac_observe(ex, 0);
+}
